93981f055a79ddf51ae1b832cf5d217441f01293
Scott Andrechek <BranchofLight@users.noreply.github.com>
2018-01-25 16:55:51 -0500

    Fixed user reported bug
    The extension now scrolls when going to a conversation window that requires scrolling that wasn't open.

--- scroll.js
+++ scroll.js
@@ -3,7 +3,6 @@ var log = document.getElementById('chatlog');
 var chatObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         if (mutation.type === "childList" && mutation.target.className.indexOf("chat_dialog_content_inner") >= 0) {
-            console.log("scrolling");
             var scroll = mutation.target.parentElement.scrollHeight;
             mutation.target.parentElement.scrollTop = scroll;
         }
@@ -11,3 +10,23 @@ var chatObserver = new MutationObserver(function(mutations) {
 });
 
 chatObserver.observe(log, { childList: true, subtree: true });
+
+document.querySelectorAll('.friendslist_entry').forEach(function(element) {
+    console.log(element);
+    element.addEventListener('click', function(e) {
+        var activeScroll;
+        var chatDialogs = document.querySelectorAll('.chat_dialog');
+        for (let i = 0; i < chatDialogs.length; i++) {
+            if (chatDialogs[i].style.display === "block") {
+                console.log("Found active scroll");
+                activeScroll = chatDialogs[i].querySelector('.chat_dialog_scroll');
+                break;
+            }
+        }
+
+        if (activeScroll) {
+            console.log(activeScroll);
+            activeScroll.scrollTop = activeScroll.scrollHeight;
+        }
+    });
+});
